ed3b201f2be67c6578d6c77d490a900aa8ef254e
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-17 10:22:47 +0900

    loading중 나오게 하기

--- frontend/src/app/list/page.tsx
+++ frontend/src/app/list/page.tsx
@@ -1,32 +1,32 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
-interface Post {
-    id: number,
-    title: string
-}
-
 export default function Home() {
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
 
-    const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/api/v1/posts")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                setPosts(data);
-            });
-    }, []);
-
-    return (
+  return (
+    <div className="flex flex-col gap-9">
+      <h1>글 목록</h1>
+      {posts.length === 0 && <div>Loading...</div>}
+      {posts.length > 0 && (
         <ul>
-            {posts.map((post) => (
-                <li key={post.id} className="p-2">
-                    {post.id}. {post.title}
-                </li>
-            ))}
+          {posts.map((post) => (
+            <li key={post.id}>
+              {post.id} : {post.title}
+            </li>
+          ))}
         </ul>
-    );
+      )}
+    </div>
+  );
 }
